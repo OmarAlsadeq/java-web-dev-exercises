df01b6f8fd3874bd997122cafa40198c538b061d
OmarAlsadeq <109781174+OmarAlsadeq@users.noreply.github.com>
2022-09-21 19:42:10 -0500

    "solved"

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,17 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a radius: ");
+        double radius = in.nextDouble();
+        while (radius <0) {
+            System.err.println("Radius must be non-negative.");
+            System.out.print(1);
+            radius = in.nextDouble();
+        }
+        System.out.println("The area of a circle of radius " + radius + " is " + Circle.getArea(radius));
+    }
 }
