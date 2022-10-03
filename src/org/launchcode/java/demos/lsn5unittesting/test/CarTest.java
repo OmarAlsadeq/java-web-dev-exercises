6cfc083d25e8953bc4c1b2ac0835074aaed4610c
OmarAlsadeq <109781174+OmarAlsadeq@users.noreply.github.com>
2022-10-03 17:10:30 -0500

    "exercises for unit testing"

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -5,29 +5,46 @@ import org.junit.Test;
 import org.launchcode.java.demos.lsn5unittesting.main.Car;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class CarTest {
 
     private Car car;
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    Car test_car;
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "prius", 10, 50);
+    }
     @Test
     public void emptyTest () {
         assertEquals(10, 10, 0.01);
     }
     //TODO: constructor sets gasTankLevel properly
     @Test
-    public void constructorSetsGasTankLevel() {
-
-        assertEquals(18.0, car.getGasTankLevel());
+    public void testInitialGasTank() {
+        assertEquals(10, test_car.getGasTankLevel(), .001);
     }
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
     //TODO: gasTankLevel is accurate after driving within tank range
     @Test
-    public void gasTankLevelAfter100Miles() {
-        car.drive(100);
-        assertEquals(15.5, car.getGasTankLevel());
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(501);
+        assertEquals(test_car.getGasTankLevel(), 0, .001);
     }
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more in tank than the size of the tank");
+    }
     //TODO: can't have more gas than tank size, expect an exception
 
 }
